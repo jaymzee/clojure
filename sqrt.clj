34e5bc59ccf1f2ecc90e5b4e82b9ee9e216ff8cf
jaymzee <jaymzee@users.noreply.github.com>
2020-11-26 06:00:07 -0500

    sqrt more clojure idiomatic
    def and defn affect the global space only and cannot be used for
    locals and nested functions.

--- sqrt.clj
+++ sqrt.clj
@@ -1,22 +1,18 @@
 (ns clojure.examples.sqrt
   (:gen-class))
+(def tolerance 0.00001)
 (defn abs [n]
   (max n (- n)))
-(def tolerance 0.00001)
-(defn fixed-point [f first-guess]
-  (defn close-enough? [v1 v2]
-    (< (abs (- v1 v2)) tolerance))
-  (defn attempt [guess]
-    (let [next-guess (f guess)]
-      (if (close-enough? guess next-guess)
-        next-guess
-        (attempt next-guess))))
-  (attempt first-guess))
-(defn average [x y]
-  (/ (+ x y) 2))
+(defn fixed-point [f initial-guess]
+  (let [close-enough? #(< (abs (- %1 %2)) tolerance)]
+    (loop [guess initial-guess]
+      (let [next-guess (f guess)]
+        (if (close-enough? guess next-guess)
+          next-guess
+          (recur next-guess))))))
 (defn average-damp [f]
-  #(average % (f %)))
+  (letfn [(avg [x y] (/ (+ x y) 2))]
+    (fn [x] (avg x (f x)))))
 (defn sqrt [x]
   (fixed-point (average-damp #(/ x %)) 1.0))
 (println (sqrt 2.0))
-(println (Math/sqrt 2.0))
