04cea2df14e77daec70d88660a4c9642a4890e7c
jaymzee <jaymzee@users.noreply.github.com>
2020-11-27 17:32:21 -0500

    javalib: generalized reify with as-function

--- javalib/src/clojure/javalib/core.clj
+++ javalib/src/clojure/javalib/core.clj
@@ -1,14 +1,16 @@
 (ns javalib.core
   (:import javalib.data.Person)
+  (:import javalib.data.Func)
   (:gen-class))
 (defn square [x]
   (* x x))
-(defn square-function []
-  (reify javalib.data.Func
+(defn ^Func as-function [f]
+  (reify Func
     (apply [this arg]
-      (square arg))))
+      (f arg))))
 (defn -main
   "instatiate a person and say hello"
   [& args]
   (.sayHello (Person.))
-  (Person/process (square-function) 3.14))
+  (Person/process (as-function square) 3.14)
+  (Person/process (as-function inc) 3.14))
